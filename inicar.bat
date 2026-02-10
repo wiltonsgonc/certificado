a5bbeb4549518caf46defc50c703f65c6fe6b2b7
Wilton Gonçalves <wilton.goncalves@fbter.org.br>
2026-02-10 10:48:20 -0300

    Alteração na ordem dos textos

--- inicar.bat
+++ inicar.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"  
-echo Servidor HTTP iniciado. Pressione Ctrl + C para finalizar o servico.
+echo Servidor HTTP iniciado. 
 echo Para abrir a aplicacao, digite em seu navegador: localhost:8000
+echo Pressione Ctrl + C para finalizar o servico.
 python3 -m http.server
 pause
